c3195bcdee9bf842c4c590fd522d2e846a12efdf
MDLC01 <57839069+MDLC01@users.noreply.github.com>
2023-04-13 15:05:10 +0200

    Remap F3+C when held to crash the game

--- src/main/java/com/mdlc/universalshortcuts/mixin/KeyboardHandlerMixin.java
+++ src/main/java/com/mdlc/universalshortcuts/mixin/KeyboardHandlerMixin.java
@@ -4,7 +4,9 @@ import com.mdlc.universalshortcuts.Utils;
 import net.minecraft.client.KeyboardHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 
 @Mixin(KeyboardHandler.class)
@@ -16,6 +18,20 @@ public abstract class KeyboardHandlerMixin {
 
     @ModifyVariable(method = "handleDebugKeys", at = @At("HEAD"), argsOnly = true)
     private int remapDebugKey(int keyCode) {
+        return Utils.remapKeyCodeToQWERTY(keyCode);
+    }
+
+    // We do not inject in InputConstants#isKeyDown because other mods might use it with an already remapped key code.
+    // This is also why this injector is so complicated. The @Slice is there to ensure we only match the right calls.
+    @ModifyArg(
+            method = "keyPress",
+            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/InputConstants;isKeyDown(JI)Z"),
+            slice = @Slice(
+                    from = @At(value = "FIELD", target = "Lnet/minecraft/client/KeyboardHandler;debugCrashKeyTime:J", ordinal = 0),
+                    to = @At(value = "FIELD", target = "Lnet/minecraft/client/KeyboardHandler;debugCrashKeyReportedCount:J", ordinal = 0)
+            )
+    )
+    private int remapIsKeyDownArgument(int keyCode) {
         return Utils.remapKeyCodeToQWERTY(keyCode);
     }
 }
